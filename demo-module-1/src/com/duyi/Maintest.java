655d6076b8d0d94b7da656bc7b52ec28ab89a9fb
Ryan-Jony <2211817972@qq.com>
2020-10-30 16:24:44 +0800

    Merge branch 'test' into main
    # Conflicts:
    #	demo-module-1/src/com/duyi/Maintest.java

--- demo-module-1/src/com/duyi/Maintest.java
+++ demo-module-1/src/com/duyi/Maintest.java
@@ -8,10 +8,5 @@ public class Maintest {
     public static void main(String[] args) {
         System.out.println("hello git");
 
-        System.out.println("hello master");
-
-        System.out.println("hello test");
-        System.out.println("布丁熊.菲特尼斯");
-        System.out.println("hello world");
     }
 }
