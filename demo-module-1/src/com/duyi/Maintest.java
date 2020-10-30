7cb17cb0fc5fd7247fd3a2749b1c05f13a74b0bc
Ryan-Jony <2211817972@qq.com>
2020-10-30 15:58:18 +0800

    master修改  增加hello master

--- demo-module-1/src/com/duyi/Maintest.java
+++ demo-module-1/src/com/duyi/Maintest.java
@@ -7,5 +7,7 @@ package com.duyi;
 public class Maintest {
     public static void main(String[] args) {
         System.out.println("hello git");
+
+        System.out.println("hello master");
     }
 }
